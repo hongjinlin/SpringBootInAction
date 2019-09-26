1855a3dda3f843acc146ab7f7def2e73464bacb5
hongjl <89757630@qq.com>
2019-09-26 21:35:26 +0800

    security

---
+++ src/main/java/com/hongjl/demo/ReaderRepository.java
@@ -0,0 +1,10 @@
+package com.hongjl.demo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * @Author hongjinlin
+ * @create 2019-09-26 21:02
+ */
+public interface ReaderRepository extends JpaRepository<Reader, String> {
+}
